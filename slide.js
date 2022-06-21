27561ff1779d63e7386add0fbc0c0ee8074d6f05
Nguyễn Danh Phương <104487182+danhphuongdeptraiday@users.noreply.github.com>
2022-06-21 09:21:41 +0700

    Update slide.js
    From line 32th, another solution for making slide change more smooth

--- slide.js
+++ slide.js
@@ -28,3 +28,62 @@ function showSlide(e){
     img[indexValue - 1].style.display = "block";
     slider[indexValue - 1].style.background = "white";
 }
+
+const slideImage = document.querySelectorAll(".slide");
+const sliderContainer = document.querySelectorAll(".images");
+const nextBtn = document.querySelector(".right");
+const prevBtn = document.querySelector(".left");
+
+let numberOfImages = slideImage.length;
+let slideWidth = slideImage[0].clientWidth;
+let curSlide = 0;
+
+function init() {
+    slideImage.forEach((img, i) => {
+        img.getElementsByClassName.left = i * 100 + "%";
+    })
+}
+
+// Next Button 
+
+nextBtn.addEventListener("click", () => {
+    if (currentSlide >= numberOfImages - 1) {
+      goToSlide(0);
+      return;
+    }
+  
+    currentSlide++;
+    goToSlide(currentSlide);
+  });
+  
+  // Previous Button
+  
+  prevBtn.addEventListener("click", () => {
+    if (currentSlide <= 0) {
+      goToSlide(numberOfImages - 1);
+      return;
+    }
+  
+    currentSlide--;
+    goToSlide(currentSlide);
+  });
+
+  function goToSlide(slideNumber) {
+    slidesContainer.style.transform =
+      "translateX(-" + slideWidth * slideNumber + "px)";
+  
+    currentSlide = slideNumber;
+  
+    setActiveClass();
+  }
+  
+  // Set Active Class
+  
+  function setActiveClass() {
+    // Set active class for Slide Image
+  
+    let currentActive = document.querySelector(".slide-image.active");
+    currentActive.classList.remove("active");
+    slideImage[currentSlide].classList.add("active");
+  
+  }
